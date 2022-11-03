3d425c9342252916f833925a6c11197cc9be0054
Rafael Leonardi <rafael.leonardi98@gmail.com>
2022-11-03 17:45:02 +0100

    Display None schermo - prompt inserimento numeri

--- main.js
+++ main.js
@@ -9,10 +9,12 @@ let playButtom = document.getElementById("playButtom");
 
 
 playButtom.addEventListener("click", function play() {
+    // Pulizia dell'array ad ogni gioco
     arrayNumCasuali.splice(0, 5);
-    let i = 0;
+    areaNumeriCasuali.innerHTML = "";
 
-    // ^Generiamo a schermo i 5 numeri casuali
+    let i = 0;
+    // ^Generiamo i 5 numeri casuali e li mettiamo nel array
     while (i < 5) {
 
 
@@ -30,19 +32,33 @@ playButtom.addEventListener("click", function play() {
     }
     console.log(arrayNumCasuali);
 
+    //^ Stampa a schermo dei numeri casuali
     for (let a = 0; a < 5; a++) {
         let div = document.createElement("div");
-        div.classList.add("classNumCasuali"); 
+        div.classList.add("classNumCasuali");
 
         div.innerText = arrayNumCasuali[a];
-        areaNumeriCasuali.append(div)
-        console.log(arrayNumCasuali[a])
-        console.log(div)
-
+        areaNumeriCasuali.append(div);
     }
 
+    function funzioneDisplayNone() {
+        let figli = document.getElementById("areaNumeriCasuali").childNodes;
+        for (let k = 0; k < figli.length; k++){
+            figli[k].classList.add("displayNone");
+        }
+            
+    }
 
+    setTimeout (function(){
+        funzioneDisplayNone()
+    }, 2500);
+
+
+    setTimeout(function () {
+        for (let b = 0; b < 5; b++) {
+            arrayNumUtente[b] = prompt("Inserisci i numeri che hai visto a schermo")
+        }
+    }, 3000);
 
 
 })
-
